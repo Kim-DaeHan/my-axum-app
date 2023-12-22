0cc560948d22bfd60e2d436106d0471f04a97af3
Kim-DaeHan <Kim-DaeHan@users.noreply.github.com>
2023-12-22 16:31:37 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,15 @@
 use axum::{routing::get, Router};
-use diesel::{
-    r2d2::{ConnectionManager, Pool},
-    PgConnection, RunQueryDsl,
-};
+use diesel::RunQueryDsl;
+use my_axum_app::*;
 use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
 
-    // 데이터베이스 연결 풀 설정
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL not set");
-    println!("111");
-    let manager = ConnectionManager::<PgConnection>::new(database_url);
-    println!("2222");
-    let pool = Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool");
-    println!("333");
+    let connection = &mut establish_connection();
 
-    // Get a connection from the pool
-    let mut connection = pool.get().expect("Failed to get connection from pool");
-
-    // Use the connection to execute a simple query
-    match diesel::sql_query("SELECT 1").execute(&mut connection) {
+    match diesel::sql_query("SELECT 1").execute(connection) {
         Ok(_) => println!("Database connection successful!"),
         Err(err) => eprintln!("Error connecting to the database: {:?}", err),
     }
