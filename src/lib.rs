3645b9b4a05abdded916c10548ee2bebd34d33f5
Kim-DaeHan <Kim-DaeHan@users.noreply.github.com>
2023-12-22 18:58:33 +0900

    .

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-// pub mod models;
-// pub mod schema;
+pub mod models;
+pub mod schema;
 
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
@@ -7,6 +7,7 @@ use std::env;
 
 pub fn establish_connection() -> PgConnection {
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    println!("{}", database_url);
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
